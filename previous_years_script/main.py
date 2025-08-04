5be91b31a9fa66e7922cca85747440e06198d55b
Erik Loma <erik.loma1998@gmail.com>
2025-08-04 19:45:39 +0200

    Removed comments

--- previous_years_script/main.py
+++ previous_years_script/main.py
@@ -2,35 +2,7 @@ import requests
 import zipfile
 import os
 import pandas as pd
-
-CURRENT_DIR = './'
-
-download_map = {
-    "2024": "https://datos.madrid.es/egob/catalogo/201200-10306320-calidad-aire-horario.zip",
-    "2023": "https://datos.madrid.es/egob/catalogo/201200-10306319-calidad-aire-horario.zip",
-    "2022": "https://datos.madrid.es/egob/catalogo/201200-10306318-calidad-aire-horario.zip",
-    "2021": "https://datos.madrid.es/egob/catalogo/201200-10306317-calidad-aire-horario.zip",
-    "2020": "https://datos.madrid.es/egob/catalogo/201200-10306316-calidad-aire-horario.zip",
-    "2019": "https://datos.madrid.es/egob/catalogo/201200-42-calidad-aire-horario.zip",
-    "2018": "https://datos.madrid.es/egob/catalogo/201200-10306314-calidad-aire-horario.zip",
-    "2017": "https://datos.madrid.es/egob/catalogo/201200-10306313-calidad-aire-horario.zip",
-    "2016": "https://datos.madrid.es/egob/catalogo/201200-28-calidad-aire-horario.zip",
-    "2015": "https://datos.madrid.es/egob/catalogo/201200-27-calidad-aire-horario.zip",
-    "2014": "https://datos.madrid.es/egob/catalogo/201200-26-calidad-aire-horario.zip",
-    "2013": "https://datos.madrid.es/egob/catalogo/201200-23-calidad-aire-horario.zip",
-    "2012": "https://datos.madrid.es/egob/catalogo/201200-22-calidad-aire-horario.zip",
-    "2011": "https://datos.madrid.es/egob/catalogo/201200-21-calidad-aire-horario.zip",
-    "2010": "https://datos.madrid.es/egob/catalogo/201200-20-calidad-aire-horario.zip",
-    "2009": "https://datos.madrid.es/egob/catalogo/201200-19-calidad-aire-horario.zip",
-    "2008": "https://datos.madrid.es/egob/catalogo/201200-18-calidad-aire-horario.zip",
-    "2007": "https://datos.madrid.es/egob/catalogo/201200-17-calidad-aire-horario.zip",
-    "2006": "https://datos.madrid.es/egob/catalogo/201200-16-calidad-aire-horario.zip",
-    "2005": "https://datos.madrid.es/egob/catalogo/201200-15-calidad-aire-horario.zip",
-    "2004": "https://datos.madrid.es/egob/catalogo/201200-14-calidad-aire-horario.zip",
-    "2003": "https://datos.madrid.es/egob/catalogo/201200-13-calidad-aire-horario.zip",
-    "2002": "https://datos.madrid.es/egob/catalogo/201200-30-calidad-aire-horario.zip",
-    "2001": "https://datos.madrid.es/egob/catalogo/201200-29-calidad-aire-horario.zip"
-}
+from data_information import *
 
 def download_zip_year(output_name: str) -> int:
     ret = -1
@@ -67,7 +39,32 @@ def read_all_csv(path: str) -> int:
 def format_csv_file(file: str) -> int:
     df = pd.read_csv(file, sep=';')
 
-    print(df.head)
+    records = []
+    row = df.iloc[0]
+
+    for hour in range(1, 25):
+        h_col = f'H{hour:02d}'      
+        value = row[h_col]
+        
+        if hour < 24:
+            timestamp = pd.Timestamp(year=row['ANO'], month=row['MES'], day=row['DIA'], hour=hour)
+        else:
+            base_date = pd.Timestamp(year=row['ANO'], month=row['MES'], day=row['DIA'])
+            timestamp = base_date + pd.Timedelta(days=1)
+
+        records.append({
+            'timestamp': timestamp,
+            'value': value,
+            'PROVINCIA': row['PROVINCIA'],
+            'MUNICIPIO': row['MUNICIPIO'],
+            'ESTACION': row['ESTACION'],
+            'MAGNITUD': row['MAGNITUD'],
+            'PUNTO_MUESTREO': row['PUNTO_MUESTREO']
+        })
+
+    df_long = pd.DataFrame(records)
+
+    print(df_long.iloc[23])
 
 
 
